679770c5efb768a1810f7151eb0e2659cf6aff57
Jhonny Claure <claurejhonny188@gmail.com>
2025-10-22 23:07:48 +0200

    update view form_qr.js

--- ports_and_zones/controllers/main.py
+++ ports_and_zones/controllers/main.py
@@ -9,7 +9,24 @@ class PropetiesController(http.Controller):
     @http.route("/", tupe="http", auth="public", website=True)
     def homePage(self, **kw):
         return request.render("ports_and_zones.homeTemplate")
-    
+
     @http.route("/qr-scan", type="http", auth="public", website=True)
     def qrScan(self, **kw):
         return request.render("ports_and_zones.qrScanTemplate")
+
+    @http.route(
+        "/qr-scan-data",
+        auth="public",
+        methods=["POST"],
+        csrf=False,
+        type="json",
+        cors="*",
+    )
+    def submitQrScan(self, **kw):
+        data = request.get_json_data()
+        logger.info("HOLA MUNDO: %s", data)
+        return {
+            "success": True,
+            "message": "Datos QR recibidos correctamente",
+            "received_data": data,
+        }
--- ports_and_zones/static/src/js/form_qr.js
+++ ports_and_zones/static/src/js/form_qr.js
@@ -1,23 +1,79 @@
-const msg = document.querySelector(".msg-log");
-const boxOptions = document.querySelector(".box-selection");
-let count = 0;
+const boxOptions = document.querySelector('.box-selection')
 
-function onScanSuccess(decodedText, decodedResult) {
-  msg.innerHTML +=
-    `${count == 0 ? "Persona: " : "Vehiculo: "}` + decodedText + "<br>";
-  console.log("Scan result: ", decodedResult);
+let msg = document.querySelector('.msg-log')
+let count = 0
 
-  if (count == 1) {
-    html5QrcodeScanner.clear();
-    boxOptions.style.display = "flex";
-  }
-
-  count += 1;
+const data = {
+  name: '',
+  vehicle: '',
 }
 
-var html5QrcodeScanner = new Html5QrcodeScanner("reader", {
+const sendScanData = async () => {
+  try {
+    const response = await fetch('/qr-scan-data', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+
+    if (response.ok) {
+      const result = await response.json()
+
+      msg.innerHTML +=
+        '<span style="color: green;">✅ Datos enviados a Odoo.</span><br>'
+    } else {
+      msg.innerHTML += `<span style="color: red;">❌ Error HTTP: ${response.status}</span><br>`
+    }
+  } catch (error) {
+    msg.innerHTML += `<span style="color: red;">❌ Error de red: ${error.message}</span><br>`
+  }
+}
+
+function onScanSuccess(decodedText, decodedResult) {
+  let currentLabel = count === 0 ? 'Persona: ' : 'Vehículo: '
+  msg.innerHTML += currentLabel + decodedText + '<br>'
+
+  count === 0 ? (data.name = decodedText) : (data.vehicle = decodedText)
+
+  count++
+
+  if (count === 2) {
+    html5QrcodeScanner.clear()
+    boxOptions.style.display = 'flex'
+
+    sendScanData()
+  }
+}
+
+var html5QrcodeScanner = new Html5QrcodeScanner('reader', {
   fps: 10,
   qrbox: 250,
-});
+})
 
-html5QrcodeScanner.render(onScanSuccess);
+html5QrcodeScanner.render(onScanSuccess)
+
+// Estilos
+const divReader = document.querySelector('#reader')
+divReader.classList.add('border-0')
+
+const btnPermissionReader = document.querySelector(
+  '#html5-qrcode-button-camera-permission'
+)
+btnPermissionReader.classList.add('py-2', 'px-4', 'btn-primary', 'rounded')
+btnPermissionReader.textContent = 'Permiso Camara'
+
+const btnUploadReader = document.querySelector(
+  '#html5-qrcode-button-file-selection'
+)
+btnUploadReader.classList.add('py-2', 'px-4', 'btn-primary', 'rounded')
+
+const iconReader = document.querySelector('#reader > div img')
+iconReader.classList.add('d-none')
+
+const spanImgReader = document.querySelector(
+  '#html5-qrcode-anchor-scan-type-change'
+)
+
+spanImgReader.classList.add('d-none')
